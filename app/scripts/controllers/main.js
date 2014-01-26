68c853116f0e5fa913d2af173b8313dc6fc0c2b7
Justin Wong <stryderjzw@gmail.com>
2014-01-25 21:39:25 -0800

    got graph working with data source

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,28 +4,24 @@ angular.module('cartistsTimelineDemoApp')
   .controller('MainCtrl', ['$scope', function ($scope) {
 
     $scope.timelineEntries = [
-      { name: 'aatrox', startDate: new Date(), endDate: new Date() },
-      { name: 'amumu', startDate: new Date(), endDate: new Date() },
-      { name: 'annie', startDate: new Date(), endDate: new Date() },
+      { name: 'aatrox', startDate: new Date(1985, 2), endDate: new Date(2014, 2) },
+      { name: 'amumu', startDate: new Date(1975, 12), endDate: new Date(2014, 8) },
+      { name: 'annie', startDate: new Date(1920, 5), endDate: new Date(2013, 3) },
     ];
-
-    console.log(_.values($scope.timelineEntries[0]));
 
     var container = document.getElementById('timeline');
 
     var chart = new google.visualization.Timeline(container);
 
   	var dataTable = new google.visualization.DataTable();
-
-  	dataTable.addColumn({ type: 'string', id: 'President' });
+  	dataTable.addColumn({ type: 'string', id: 'Artist' });
   	dataTable.addColumn({ type: 'date', id: 'Start' });
   	dataTable.addColumn({ type: 'date', id: 'End' });
 
-  	dataTable.addRows([
-  	[ 'Washington', new Date(1789, 3, 29), new Date(1797, 2, 3) ],
-  	[ 'Adams',      new Date(1797, 2, 3),  new Date(1801, 2, 3) ],
-  	[ 'Jefferson',  new Date(1801, 2, 3),  new Date(1809, 2, 3) ]]);
+    var data = _.map($scope.timelineEntries, function(entry) {
+      return _.values(entry);
+    });
 
+    dataTable.addRows(data);
   	chart.draw(dataTable);
-
   }]);
